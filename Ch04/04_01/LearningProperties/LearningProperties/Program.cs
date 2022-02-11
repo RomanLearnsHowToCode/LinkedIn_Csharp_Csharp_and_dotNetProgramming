fd51082a5fccc9a367423d6d4a0f2fa64e980e0f
Roman <roman.liewehr@hotmail.com>
2022-02-11 16:45:06 +0000

    Chapter 04 done, Starting Chapter 05

--- Ch04/04_01/LearningProperties/LearningProperties/Program.cs
+++ Ch04/04_01/LearningProperties/LearningProperties/Program.cs
@@ -4,11 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * A property is a member that provides a mechanism to read, write, and compute the value of a private field.
+ * Properties seem to act like public variables, but they are more like functions in a way because they
+ * contain special methods called accessors, which are the getters and setters of the property. 
+ */ 
+
+/*
+* The nice thing about properties, is we have more control over access. Since I did not define a setter,
+* if we tried to set isAlive, we will get an error since this is not possible. Unlike a public variable which we
+* can set and possibly break our application. 
+ */
 namespace LearningProperties
 {
     class Player
     {
-        public bool isAlive = true;
+        // Let's conver isAlive into property
+        //public bool isAlive = true;
+
+        public bool IsAlive {// naming convention for properties = capital I
+            get {return health > 0; }
+        } 
+
         int health = 100;
 
         public void Hit()
@@ -17,22 +34,31 @@ namespace LearningProperties
             health -= r.Next(5, 50);
         }
 
+        // We don't need this function anymore, since the logic is in property function
+        /*
         public void CheckDeath()
         {
             if (health <= 0)
                 isAlive = false;
         }
+        */
     }
     class Program
     {
         static void Main(string[] args)
         {
             Player player = new Player();
+
+            //setter will throw error
+            //player.IsAlive = false;
+
             for (int i = 0; i < 20; i++)
             {
                 player.Hit();
-                player.CheckDeath();
-                Console.WriteLine("Is player alive: " + player.isAlive);
+                //player.CheckDeath(); // we don't need this check anymore
+                //Console.WriteLine("Is player alive: " + player.isAlive);
+                //We need to update the console log and log IsAlive..
+                Console.WriteLine("Is player alive: " + player.IsAlive);
             }
         }
     }
--- Ch04/04_02/LearningAutoProperties/LearningAutoProperties/Program.cs
+++ Ch04/04_02/LearningAutoProperties/LearningAutoProperties/Program.cs
@@ -4,20 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Auto implemented properties are a more compact form of the manually implemented properties
+/*
+One advantage I can think of is that if you have a field with lots of code depending on it, and then you realize
+you need some functionality to execute with setting or getting the value, then you will have to rebuild any
+code that is in a separate assembly, that is trying to look for that field, if you want to switch it to a
+property. In other words, it will cause breaking changes in your app. So it's better to use it from the get-go,
+even if you don't need that functionality initially.  
+ */
 namespace LearningProperties
 {
     class Player
     {
         public bool IsAlive
         {
-            get { return health > 0; }
+            //get { return health > 0; }
+            get { return Health > 0; }
         }
+
+        //public int Health { get; } = 100; -> This won't work
+        //public int Health { get; set; } = 100;
+        public int Health { get; private set; } = 100;
+
+        /*
         int health = 100;
+
+        // we can publicaly retrieve health (which is private property)
+        public int Health
+        {
+            get { return health; }
+        }
+        */
 
         public void Hit()
         {
             Random r = new Random();
-            health -= r.Next(5, 50);
+            //health -= r.Next(5, 50);
+            Health -= r.Next(5, 50);
         }
     }
     class Program
