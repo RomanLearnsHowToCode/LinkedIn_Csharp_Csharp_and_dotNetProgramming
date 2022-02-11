6fcd851cba50f7c275387bfb58ab60bd1ccd020e
Roman <roman.liewehr@hotmail.com>
2022-02-11 17:05:40 +0000

    Chapter 06 is done, Starting Chapter 07

--- Ch06/06_01/LearningNulls/LearningNulls/Program.cs
+++ Ch06/06_01/LearningNulls/LearningNulls/Program.cs
@@ -4,14 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+ * Using null coalescing, if we try to set a variable to something that happens to be null,
+ * it will instead be set to a default value.
+ */
+
+
+/*
+* Let's imagine we were accessing the database and most of the user records have name values,
+* but one record does not have a value and so it could come back as null. Instead we could put the 
+* default value of empty or not available. An important note is that null coalescing does not work with
+* value types because value types cannot be null unless we specify it explicitly
+*/
+
+
 namespace LearningNulls
 {
     class Program
     {
         static void Main(string[] args)
         {
+            /*
+             * We want to use null coalescing to set a default value. We can do that with a double question
+             * mark syntax followed by the default value.
+             */
+
+            /*
+             
+             */
+
             int? s = null; //"hello";
-            int s2 = s ?? 2;
+            int s2 = s ?? 2; // default value is 2, is the value null integer? If not then use default = 2
 
             Console.WriteLine(s2);
         }
--- Ch06/06_02/LearningNullConditional/LearningNullConditional/Program.cs
+++ Ch06/06_02/LearningNullConditional/LearningNullConditional/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Dealing with NULL
 namespace LearningNullConditional
 {
     class Person
@@ -14,8 +15,23 @@ namespace LearningNullConditional
     {
         static void Main(string[] args)
         {
-            Person person = new Person() { Name = "Tim" }; //what if this was null
-            Console.WriteLine(person.Name);
+
+            Person person = null;
+
+            if(person != null)
+            {
+                Console.WriteLine(person?.Name);
+            } else {
+                Console.WriteLine("Person not found");
+            }
+
+            Person person2 = new Person() { Name = "Tim" }; //what if this was null
+            // Person2 name lenght is 3 as expected
+            Console.WriteLine("Person2 name lenght is " + person2?.Name.Length);
+
+            //If it is not declared then it will result in error, Name? will help here
+            Person person3= new Person();
+            Console.WriteLine("Person 3 name length is" + person3?.Name?.Length);
         }
     }
 }
